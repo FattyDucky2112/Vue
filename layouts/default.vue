56973ba284b6b44bc1bf2b52c20697358332231b
FattyDucky2112 <fattyducky@hotmail.com>
2020-01-26 18:22:21 +0100

    navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <ul>
+    <ul class="navlist">
       <li><nuxt-link to="/">Eintrag 1</nuxt-link></li>
-      <li><nuxt-link to="/admin">admin</nuxt-link></li>
+      <li><nuxt-link to="/admin">Admin</nuxt-link></li>
     </ul>
     <nuxt />
   </div>
@@ -26,6 +26,40 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
+}
+
+
+.navlist{
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  height: 60px;
+  background-color: #82ecd4;
+  opacity: 0.6;
+}
+
+.navlist li{
+  float: left;
+  background-color: #82ecd4;
+  height: 100%;
+  opacity: 0.6;
+}
+
+.navlist li:hover{
+  float: left;
+  background-color: #82ecd4;
+  height: 100%;
+  opacity: 1; 
+}
+
+.navlist li a{
+  display: block;
+  color: rgb(0, 0, 0);
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 20px;
+  
 }
 
 .button--green {
